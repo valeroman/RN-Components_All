6cfc56bf1b72dc5c335dd8ffffc54796c6e929a7
Roman Valero <valeroman@gmail.com>
2022-10-01 19:17:41 -0500

    Proyecto base Stack-Icon

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/navigator/Navigator';
 
-
 const App = () => {
   return (
     <NavigationContainer>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,13 +3,14 @@ import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const HomeScreen = () => {
-  return (
-    <View>
-        <Icon 
-            name='star-outline'
-            color='black'
-            size={ 50 }
-        />
-    </View>
-  )
+
+    return (
+        <View>
+            <Icon 
+                name='star-outline'
+                color='black'
+                size={ 50 }
+            />
+        </View>
+    )
 }
